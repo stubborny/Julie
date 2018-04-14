f4c487ccfe8e702f3d1a365c40ba392a9bfb69a5
jyhe <15301144@bjtu.edu.cn>
2018-04-14 11:39:56 +0800

    缴纳押金和学生认证界面

---
+++ app/src/main/java/com/bjtu/julie/Activity/QualificationedActivity.java
@@ -0,0 +1,16 @@
+package com.bjtu.julie.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bjtu.julie.R;
+
+
+public class QualificationedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_qualificationed);
+    }
+}
